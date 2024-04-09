27e8e37ae92adde3ea9bf6b1766faf487385d394
Guillermo Woivre Gomez <guillermowoivre@gmail.com>
2024-04-09 18:33:01 +0200

    hitcount

--- assets/2-hitcounter/bin/ceu-cdk-workshop.ts
+++ assets/2-hitcounter/bin/ceu-cdk-workshop.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { CeuCdkWorkshopStack } from '../lib/ceu-cdk-workshop-stack';
+import { CeuCdkWorkshopStack } from '../../../lib/ceu-cdk-workshop-stack';
 import { defaultStackSynthesizer } from '../lib/custom-stack-synthesizer';
 
 const app = new cdk.App();
--- lib/ceu-cdk-workshop-stack.ts
+++ lib/ceu-cdk-workshop-stack.ts
@@ -3,20 +3,26 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Role } from 'aws-cdk-lib/aws-iam';
+import { HitCounter } from './hitcounter';
 
 export class CeuCdkWorkshopStack extends Stack {
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      code: lambda.Code.fromAsset('lambda/hello-world'),
-      handler: 'index.handler',
-      role: Role.fromRoleName(this, 'Role', 'LabRole')
+      runtime: lambda.Runtime.NODEJS_20_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda/hello-world'),  // code loaded from "lambda" directory
+      handler: 'index.handler',                // file is "hello", function is "handler"
+      role: Role.fromRoleName(this, 'Role', 'LabRole'),
+    });
+
+    const hitcounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
     });
 
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+      handler: hitcounter.handler
     });
 
   }
